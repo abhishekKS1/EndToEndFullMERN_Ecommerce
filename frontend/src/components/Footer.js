d8e17450bfc2ebec6db776742550ee9830b9bfd2
Abhishek Kumar Sharma <152519777+abhishekKS1@users.noreply.github.com>
2025-05-14 15:39:28 +0530

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,10 +4,10 @@ const Footer = () => {
    return (
       <footer className="bg-slate-200">
          <div className="container mx-auto p-4">
-            <p className="text-center font-bold" title="shop fast">        
+            <pre className="text-center font-bold" title="shop fast">        
       Thank you for choosing us!!{" "}
    © 2025 Abhishek Kumar Sharma. All rights reserved.
-            </p>
+            </pre>
          </div>
       </footer>
    );
