ceae960c97ceebf2a0988f3c9d45b7659592c9cb
Abhishek Kumar Sharma <152519777+abhishekKS1@users.noreply.github.com>
2025-05-14 15:35:43 +0530

    Update Footer.js add copyright

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,8 +4,9 @@ const Footer = () => {
    return (
       <footer className="bg-slate-200">
          <div className="container mx-auto p-4">
-            <p className="text-center font-bold" title="shop fast">
-               Thank you for choosing us!!{" "}
+            <p className="text-center font-bold" title="shop fast">        
+      Thank you for choosing us!!{" "}
+   © 2025 Abhishek Kumar Sharma. All rights reserved.
             </p>
          </div>
       </footer>
